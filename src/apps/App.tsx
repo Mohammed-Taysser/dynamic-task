12d3ed95517d7d6b2dc8e1b218f9dfa9af1bac87
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-22 15:01:05 +0300

    create app

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,12 +1,19 @@
-import Button from '@mui/material/Button';
+import { CssBaseline, LinearProgress, ThemeProvider } from '@mui/material';
+import { Suspense } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import routes from './Routes';
+import theme from './Theme';
 
 import '../assets/scss/app.scss';
 
 function App() {
   return (
-    <div className='app'>
-      <Button variant='contained'>Hello world</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Suspense fallback={<LinearProgress />}>
+        <RouterProvider router={routes} fallbackElement={<LinearProgress />} />
+      </Suspense>
+    </ThemeProvider>
   );
 }
 
