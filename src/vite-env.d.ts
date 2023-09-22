bc984ad69b3b46a38a5acade68c4cc452ef8a257
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-22 15:35:42 +0300

    add login types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,26 @@ type LocalStorageKeys = 'refreshToken' | 'accessToken';
 type LocalStorageKeysObject = {
   [key in LocalStorageKeys]: string;
 };
+
+interface LoginResponse {
+  refresh: string;
+  access: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  is_superuser: boolean;
+  is_staff: boolean;
+}
+
+type RequestStatus = 'idle' | 'loading' | 'succeeded' | 'failed';
+
+interface LoginBodyState {
+  email: string;
+  password: string;
+}
+
+interface RequestState<T = null> {
+  data: T;
+  status: RequestStatus;
+  error: SerializedError;
+}
