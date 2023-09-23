277fcbce6a60327262d6e81a9542500d46504ecf
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-23 15:09:25 +0300

    add redux store

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,19 +1,26 @@
 import { CssBaseline, LinearProgress, ThemeProvider } from '@mui/material';
 import { Suspense } from 'react';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import store from '../redux/store';
 import routes from './Routes';
 import theme from './Theme';
 
-import '../assets/scss/app.scss';
+import '../assets/css/app.css';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Suspense fallback={<LinearProgress />}>
-        <RouterProvider router={routes} fallbackElement={<LinearProgress />} />
-      </Suspense>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Suspense fallback={<LinearProgress />}>
+          <RouterProvider
+            router={routes}
+            fallbackElement={<LinearProgress />}
+          />
+        </Suspense>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
