811a70962212a42b628b01f7dde6a85819576a9a
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-23 15:11:53 +0300

    add app types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,38 @@
 /// <reference types="vite/client" />
+/// <reference types="@material-ui/core" />
 
+// Local Storage
 type LocalStorageKeys = 'refreshToken' | 'accessToken';
 
 type LocalStorageKeysObject = {
   [key in LocalStorageKeys]: string;
 };
 
+// Button Component
+interface CustomButtonProps {
+  isLoading: boolean;
+  children: React.ReactNode;
+}
+
+// Redux state
+type RequestStatus = 'idle' | 'loading' | 'succeeded' | 'failed';
+
+interface RequestState<T = null> {
+  data: T;
+  status: RequestStatus;
+  error: SerializedError;
+}
+
+interface ResponseError {
+  detail: string;
+}
+
+// Contacts Init State
+interface ContactInitState extends RequestState<ContactsResponse> {
+  selectedContact: ContactItem | null;
+}
+
+// Login page
 interface LoginResponse {
   refresh: string;
   access: string;
@@ -16,15 +43,36 @@ interface LoginResponse {
   is_staff: boolean;
 }
 
-type RequestStatus = 'idle' | 'loading' | 'succeeded' | 'failed';
-
 interface LoginBodyState {
   email: string;
   password: string;
 }
 
-interface RequestState<T = null> {
-  data: T;
-  status: RequestStatus;
-  error: SerializedError;
+// Homepage
+interface ContactItem {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+}
+
+interface ContactsResponse {
+  links: {
+    next: string | null;
+    previous: string | null;
+  };
+  count: number;
+  results: ContactItem[];
+}
+
+interface GetContactsQueryParams {
+  [key: string]: string | number | MRT_SortingState;
+}
+
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  reFetchContacts: () => void;
+  selectedContact: ContactItem;
 }
